4b53a179cb981e4ef931d4a5d7402b642bde40c3
siddhivaity17 <siddhivaity2006@gmail.com>
2026-02-18 14:58:57 +0530

    Update Code1.py

--- Code1.py
+++ Code1.py
@@ -1,1 +1,18 @@
+from machine import Pin
+import time
 
+A = Pin(32, Pin.OUT)
+B = Pin(25, Pin.OUT)
+C = Pin(5, Pin.OUT)
+D = Pin(19, Pin.OUT)
+pos= [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+while True:
+    for i in pos:
+        A.value(i[0])
+        B.value(i[1])
+        C.value(i[2])
+        D.value(i[3])
+        time.sleep(0.005)
+        print("kuchbhi")
+        
